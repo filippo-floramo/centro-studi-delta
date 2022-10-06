8b0665ff37bf3eb4db4a651ca1ccbd3c07f48837
Filippo Floramo <filifloramo@gmail.com>
2022-10-06 16:47:31 +0200

    test for dynamic rendering and typechecking

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+export default function app(): string {
+
+   const payoff = `<h1 class="hero--payoff">Le nostre </br>
+    competenze </br> 
+    al servizio degli studenti</h1>`
+
+ 
+   return payoff
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import app from "./app"
 import './styles/styles.scss'
 
+
 console.log("test success")
+
+const payoffCont = document.getElementById("lol");
+
+
+   if(payoffCont) { payoffCont.innerHTML = app()}
+
+
